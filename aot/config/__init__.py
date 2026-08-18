08976ef7aa51aece98d7fb5c326824f9cca34e64
AoT-inc <aot.inc.kr@gmail.com>
2026-08-18 15:44:33 +0900

    sync: v26.08.11 — 지도 계층 성능·정확도 개선

--- aot/config/__init__.py
+++ aot/config/__init__.py
@@ -18,7 +18,7 @@ from config_translations import TRANSLATIONS as T
 
 MYCODO_VERSION = '8.16.0'
 ALEMBIC_VERSION = 'p6_38_geo_containment_cache_20260818'
-AOT_VERSION = '26.08.10'
+AOT_VERSION = '26.08.11'
 
 # FORCE UPGRADE MASTER
 # Set True to enable upgrading to the master branch of the AoT repository.
